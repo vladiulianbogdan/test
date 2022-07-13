54a35b63dba85b29165961082514530d2a8ecd35
Bogdan Vlad <vladbgd@gmail.com>
2022-07-13 21:46:55 +0300

    New Branch Commit - Automatically generated by Codetruck

---
+++ models/eeee.js
@@ -0,0 +1,15 @@
+/*!
+=========================================================
+* Copyright Codetruck Software (https://codetruck.io)
+* Coded by Codetruck Software
+=========================================================
+* The above copyright informs you that all code is under
+  copyright and all the intelectual property rights are owned by Codetruck Software.
+*/
+
+module.exports = (sequelize, DataTypes) => {
+	const Eeee = sequelize.define("Eeee", {
+	});
+
+	return Eeee;
+};
